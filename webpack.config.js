47d7a8fd323012cd5f15e875cf8e55d404a2e3bd
RafaelVillagomezDev <rafaelvillagomez.dev@gmail.com>
2023-04-29 18:19:12 +0200

    Subida configuracion inicial webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+
+// Webpack configuration
+module.exports = {
+    stats: { children: true },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "main.bundle-[hash].js",
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js','jsx','.html'],
+  },
+
+  mode: process.env.NODE_ENV || "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+      filename: "index.html",
+    }),
+  ],
+
+  // LOADERS
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ],
+  },
+};
